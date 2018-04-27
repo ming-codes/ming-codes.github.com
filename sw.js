ac4cf57d113fc72573215309986ab9bef01af469
Ming Liu <ming.codes@gmail.com>
2018-04-27 10:00:38 -0700

    Deployed f64c4ee

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 (function(){"use strict"
 function e(e,t){return caches.keys().then(function(n){n.forEach(function(n){var s=0===n.indexOf(e),i=n!==t
-s&&i&&caches.delete(n)})})}function t(e,t){return!!t.find(function(t){return t.test(decodeURI(e))})}self.CACHE_BUSTER="1524812536722|0.738855327826897",self.addEventListener("install",function(e){return self.skipWaiting()}),self.addEventListener("activate",function(e){return self.clients.claim()})
-var n=["assets/ming-codes-fastboot-e9b9c3bb6ccc5b8c5ae1d28a126d3fdf.js","assets/ming-codes-5b091b9b1e7c6794079c15e4cb6fc082.css","assets/ming-codes-5632127c2eaeeb48071a247adcd493f0.js","assets/vendor-87b2374ac52a549e2fae0d5b4298516c.css","assets/vendor-3c32a7d59c370a0d092b9f028ea4c001.js"],s="esw-asset-cache-1",i=n.map(function(e){return new URL(e,self.location).toString()}),c=function(){caches.open(s).then(function(e){return e.keys().then(function(t){t.forEach(function(t){-1===i.indexOf(t.url)&&e.delete(t)})})})}
+s&&i&&caches.delete(n)})})}function t(e,t){return!!t.find(function(t){return t.test(decodeURI(e))})}self.CACHE_BUSTER="1524848429053|0.23052193678356958",self.addEventListener("install",function(e){return self.skipWaiting()}),self.addEventListener("activate",function(e){return self.clients.claim()})
+var n=["assets/ming-codes-fastboot-e9b9c3bb6ccc5b8c5ae1d28a126d3fdf.js","assets/ming-codes-5b091b9b1e7c6794079c15e4cb6fc082.css","assets/ming-codes-50a40bf0e21ffa077cf4466b7f5e2930.js","assets/vendor-87b2374ac52a549e2fae0d5b4298516c.css","assets/vendor-3c32a7d59c370a0d092b9f028ea4c001.js"],s="esw-asset-cache-1",i=n.map(function(e){return new URL(e,self.location).toString()}),c=function(){caches.open(s).then(function(e){return e.keys().then(function(t){t.forEach(function(t){-1===i.indexOf(t.url)&&e.delete(t)})})})}
 self.addEventListener("install",function(e){e.waitUntil(caches.open(s).then(function(e){return Promise.all(i.map(function(t){var n=new Request(t,{mode:"cors"})
 return fetch(n).then(function(n){if(n.status>=400){var s=new Error("Request for "+t+" failed with status "+n.statusText)
 throw s}return e.put(t,n)}).catch(function(e){console.error("Not caching "+t+" due to "+e)})}))}))}),self.addEventListener("activate",function(t){t.waitUntil(Promise.all([e("esw-asset-cache",s),c()]))}),self.addEventListener("fetch",function(e){var t="GET"===e.request.method,n=-1!==i.indexOf(e.request.url)
 t&&n&&e.respondWith(caches.match(e.request,{cacheName:s}).then(function(t){return t||fetch(e.request)}))})
-var r=[],a=[]
-self.INDEX_FILE_HASH="6deb2a23f64e501dde669b53ec73eb85"
+var a=[],r=[]
+self.INDEX_FILE_HASH="3d7d95092fee5a71ed11a91da56519f6"
 var o=new URL("index.html",self.location).toString()
-self.addEventListener("install",function(e){e.waitUntil(fetch(o,{credentials:"include"}).then(function(e){return caches.open("esw-index-1").then(function(t){return t.put(o,e)})}))}),self.addEventListener("activate",function(t){t.waitUntil(e("esw-index","esw-index-1"))}),self.addEventListener("fetch",function(e){var n=e.request,s=new URL(n.url),i="GET"===n.method,c=-1!==n.headers.get("accept").indexOf("text/html"),u=s.origin===location.origin,f=t(n.url,r),d=!a.length||t(n.url,a)
+self.addEventListener("install",function(e){e.waitUntil(fetch(o,{credentials:"include"}).then(function(e){return caches.open("esw-index-1").then(function(t){return t.put(o,e)})}))}),self.addEventListener("activate",function(t){t.waitUntil(e("esw-index","esw-index-1"))}),self.addEventListener("fetch",function(e){var n=e.request,s=new URL(n.url),i="GET"===n.method,c=-1!==n.headers.get("accept").indexOf("text/html"),u=s.origin===location.origin,f=t(n.url,a),d=!r.length||t(n.url,r)
 !("/tests"===s.pathname&&!1)&&i&&c&&u&&d&&!f&&e.respondWith(caches.match(o,{cacheName:"esw-index-1"}))})})()
